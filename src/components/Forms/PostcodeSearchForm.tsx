4047cb69a6b92933bee59ef2942c664333e260db
Daniel Saleh <danielsaleh@Mac.WestWallasey.local>
2026-03-23 09:24:53 +0000

    update postcode search form component so that form does not disappear on smaller screen sizes

--- src/components/Forms/PostcodeSearchForm.tsx
+++ src/components/Forms/PostcodeSearchForm.tsx
@@ -7,138 +7,136 @@ export const PostcodeSearchForm = () => {
   const purpleTheme = useTheme();
   const { width } = useWindowDimensions();
 
-  if (width > 450) {
-    return (
-      <HorizontalFlexForm
-        $justify="center"
-        $align="center"
-        $paddingX="1"
-        $paddingY="1"
-        $widthPixels
-        $width="auto"
-        $backgroundColor="rgba(255, 255, 255, 0.8)"
-        $borderRadius={5}
-        $applyBorder
-        $borderWidth={3}
-        $borderStyle="solid"
-        $borderColor={purpleTheme.palette.primary.dark}
-        $blur
-      >
-        {width > 565 ? (
-          <>
-            <TextField
-              id="postcode-entry"
-              label="Enter Postcode"
-              required
-              autoComplete="off"
-              sx={{
-                "& .MuiInputBase-root": {
-                  height: "55px",
-                  width: "200px",
-                  color: purpleTheme.palette.primary.dark,
-                  borderColor: purpleTheme.palette.primary.dark,
-
-                  "& fieldset": {
-                    borderColor: purpleTheme.palette.primary.main,
-                  },
-
-                  "&.Mui-focused fieldset": {
-                    borderColor: purpleTheme.palette.primary.dark,
-                  },
-
-                  "&:hover fieldset": {
-                    borderColor: purpleTheme.palette.primary.dark,
-                  },
-                },
-
-                "& .MuiFormLabel-root": {
-                  color: purpleTheme.palette.primary.dark,
-                },
-
-                "& .MuiInputLabel-outlined": {
-                  color: purpleTheme.palette.primary.dark,
-                },
-
-                "& .MuiOutlinedInput-input": {
-                  color: purpleTheme.palette.primary.dark,
-                },
-              }}
-            />
-            <Button
-              type="submit"
-              sx={{
-                color: "#FFFFFF",
-                backgroundColor: purpleTheme.palette.primary.dark,
-                margin: "0em 1em",
+  return (
+    <HorizontalFlexForm
+      $justify="center"
+      $align="center"
+      $paddingX="1"
+      $paddingY="1"
+      $widthPixels
+      $width="auto"
+      $backgroundColor="rgba(255, 255, 255, 0.8)"
+      $borderRadius={5}
+      $applyBorder
+      $borderWidth={3}
+      $borderStyle="solid"
+      $borderColor={purpleTheme.palette.primary.dark}
+      $blur
+    >
+      {width > 565 ? (
+        <>
+          <TextField
+            id="postcode-entry"
+            label="Enter Postcode"
+            required
+            autoComplete="off"
+            sx={{
+              "& .MuiInputBase-root": {
                 height: "55px",
-                width: "100px",
-              }}
-            >
-              Search
-            </Button>
-          </>
-        ) : (
-          <VerticalFlexDiv
-            $align=""
-            $justify="center"
-            $paddingX="1"
-            $paddingY="1"
-            $widthPixels
-            $width="auto"
-            $backgroundColor="transparent"
-          >
-            <TextField
-              id="postcode-entry"
-              label="Enter Postcode"
-              required
-              autoComplete="off"
-              sx={{
-                "& .MuiInputBase-root": {
-                  height: "55px",
-                  width: "200px",
-                  color: purpleTheme.palette.primary.dark,
-                  borderColor: purpleTheme.palette.primary.dark,
-
-                  "& fieldset": {
-                    borderColor: purpleTheme.palette.primary.main,
-                  },
-
-                  "&.Mui-focused fieldset": {
-                    borderColor: purpleTheme.palette.primary.dark,
-                  },
-
-                  "&:hover fieldset": {
-                    borderColor: purpleTheme.palette.primary.dark,
-                  },
-                },
-
-                "& .MuiFormLabel-root": {
-                  color: purpleTheme.palette.primary.dark,
-                },
-
-                "& .MuiInputLabel-outlined": {
-                  color: purpleTheme.palette.primary.dark,
-                },
-                "& .MuiOutlinedInput-input": {
-                  color: purpleTheme.palette.primary.dark,
-                },
-              }}
-            />
-            <Button
-              type="submit"
-              sx={{
-                color: "#FFFFFF",
-                backgroundColor: purpleTheme.palette.primary.dark,
-                height: "55px",
-                marginTop: "1em",
                 width: "200px",
-              }}
-            >
-              Search
-            </Button>
-          </VerticalFlexDiv>
-        )}
-      </HorizontalFlexForm>
-    );
-  }
+                color: purpleTheme.palette.primary.dark,
+                borderColor: purpleTheme.palette.primary.dark,
+
+                "& fieldset": {
+                  borderColor: purpleTheme.palette.primary.main,
+                },
+
+                "&.Mui-focused fieldset": {
+                  borderColor: purpleTheme.palette.primary.dark,
+                },
+
+                "&:hover fieldset": {
+                  borderColor: purpleTheme.palette.primary.dark,
+                },
+              },
+
+              "& .MuiFormLabel-root": {
+                color: purpleTheme.palette.primary.dark,
+              },
+
+              "& .MuiInputLabel-outlined": {
+                color: purpleTheme.palette.primary.dark,
+              },
+
+              "& .MuiOutlinedInput-input": {
+                color: purpleTheme.palette.primary.dark,
+              },
+            }}
+          />
+          <Button
+            type="submit"
+            sx={{
+              color: "#FFFFFF",
+              backgroundColor: purpleTheme.palette.primary.dark,
+              margin: "0em 1em",
+              height: "55px",
+              width: "100px",
+            }}
+          >
+            Search
+          </Button>
+        </>
+      ) : (
+        <VerticalFlexDiv
+          $align=""
+          $justify="center"
+          $paddingX="1"
+          $paddingY="1"
+          $widthPixels
+          $width="auto"
+          $backgroundColor="transparent"
+        >
+          <TextField
+            id="postcode-entry"
+            label="Enter Postcode"
+            required
+            autoComplete="off"
+            sx={{
+              "& .MuiInputBase-root": {
+                height: "55px",
+                width: "200px",
+                color: purpleTheme.palette.primary.dark,
+                borderColor: purpleTheme.palette.primary.dark,
+
+                "& fieldset": {
+                  borderColor: purpleTheme.palette.primary.main,
+                },
+
+                "&.Mui-focused fieldset": {
+                  borderColor: purpleTheme.palette.primary.dark,
+                },
+
+                "&:hover fieldset": {
+                  borderColor: purpleTheme.palette.primary.dark,
+                },
+              },
+
+              "& .MuiFormLabel-root": {
+                color: purpleTheme.palette.primary.dark,
+              },
+
+              "& .MuiInputLabel-outlined": {
+                color: purpleTheme.palette.primary.dark,
+              },
+              "& .MuiOutlinedInput-input": {
+                color: purpleTheme.palette.primary.dark,
+              },
+            }}
+          />
+          <Button
+            type="submit"
+            sx={{
+              color: "#FFFFFF",
+              backgroundColor: purpleTheme.palette.primary.dark,
+              height: "55px",
+              marginTop: "1em",
+              width: "200px",
+            }}
+          >
+            Search
+          </Button>
+        </VerticalFlexDiv>
+      )}
+    </HorizontalFlexForm>
+  );
 };
