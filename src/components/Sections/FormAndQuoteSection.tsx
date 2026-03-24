a85449cfd1f154905207425f413727b15cabeada
Daniel Saleh <danielsaleh@Mac.WestWallasey.local>
2026-03-24 09:07:09 +0000

    add screensize hook to form and quote section

--- src/components/Sections/FormAndQuoteSection.tsx
+++ src/components/Sections/FormAndQuoteSection.tsx
@@ -2,29 +2,34 @@ import { HorizontalFlexDiv } from "../Layouts/HorizontalFlexDiv";
 import { VerticalFlexDiv } from "../Layouts/VerticalFlexDiv";
 import { ImprovementsForm } from "../Forms/ImprovementsForm";
 import { QuoteSection } from "./QuoteSection";
+import { useWindowDimensions } from "../../hooks/usewindowDimensions";
 
 export const FormAndQuoteSection = () => {
-  return (
-    <HorizontalFlexDiv
-      $align="center"
-      $justify="center"
-      $paddingX="0"
-      $paddingY="0"
-      $width="100%"
-      $height="450px"
-    >
-      <VerticalFlexDiv
-        $width="50"
+  const { width } = useWindowDimensions();
+
+  if (width > 1020) {
+    return (
+      <HorizontalFlexDiv
         $align="center"
         $justify="center"
         $paddingX="0"
         $paddingY="0"
-        $height="100%"
+        $width="100%"
+        $height="450px"
       >
-        <QuoteSection />
-      </VerticalFlexDiv>
+        <VerticalFlexDiv
+          $width="50"
+          $align="center"
+          $justify="center"
+          $paddingX="0"
+          $paddingY="0"
+          $height="100%"
+        >
+          <QuoteSection />
+        </VerticalFlexDiv>
 
-      <ImprovementsForm />
-    </HorizontalFlexDiv>
-  );
+        <ImprovementsForm />
+      </HorizontalFlexDiv>
+    );
+  }
 };
